5efb84cd6edfb8a096f65ff31ca9a80d96c63253
Anastasia1243 <zhezherya.anastasia@gmail.com>
2022-08-11 17:32:58 +0300

    slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,14 @@
 $(function () {
 
+	$('.work__items').slick({
+
+      infinite: true,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+		prevArrow: '<button type="button" class="slick-prev"><img src="img/prev-arrow.svg" alt=""></button>',
+		nextArrow: '<button type="button" class="slick-next"><img src="img/next-arrow.svg" alt=""></button>',
+
+	});
+
+
 })
