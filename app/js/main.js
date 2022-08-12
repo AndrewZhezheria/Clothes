a2b4afc65dad4398a2d52332cdf9cc16f0e5dee7
Anastasia1243 <zhezherya.anastasia@gmail.com>
2022-08-12 21:42:59 +0300

    new

--- app/js/main.js
+++ app/js/main.js
@@ -3,7 +3,6 @@ $(function () {
 	$('.work__items').slick({
 		autoplay: true,
       autoplaySpeed: 2000,
-		infinite: false,
 		slidesToShow: 3,
 		slidesToScroll: 1,
 		prevArrow: '<button type="button" class="slick-prev"><img src="img/prev-arrow.svg" alt=""></button>',
