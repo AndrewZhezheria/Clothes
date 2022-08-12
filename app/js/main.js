2d6b8a68d3fb17a2b16cedda0550b89592066492
Anastasia1243 <zhezherya.anastasia@gmail.com>
2022-08-12 15:41:38 +0300

    order

--- app/js/main.js
+++ app/js/main.js
@@ -1,10 +1,11 @@
 $(function () {
 
 	$('.work__items').slick({
-
-      infinite: true,
-      slidesToShow: 3,
-      slidesToScroll: 1,
+		autoplay: true,
+      autoplaySpeed: 2000,
+		infinite: false,
+		slidesToShow: 3,
+		slidesToScroll: 1,
 		prevArrow: '<button type="button" class="slick-prev"><img src="img/prev-arrow.svg" alt=""></button>',
 		nextArrow: '<button type="button" class="slick-next"><img src="img/next-arrow.svg" alt=""></button>',
 
